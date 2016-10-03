b879ee872a55c89517abe9ee114337e926342508
103021043 <103021043@live.asia.edu.tw>
2016-10-03 17:48:29 +0800

    hw00繳交

---
+++ src/ex/hw00.java
@@ -0,0 +1,23 @@
+package ex;
+/*
+ * Topic:自我介紹
+ * Date: 2016/09/12
+ * Author: 103021043 何宙運
+ */
+import java.util.Scanner;
+public class hw00 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn=new Scanner(System.in);
+System.out.println("姓名:何宙運");
+System.out.println("英文名字:RIKU");
+System.out.println("性別:男");
+System.out.println("生日:85/04/21");
+System.out.println("星座:Taurus");
+System.out.println("興趣:聽音樂&繪畫&閱讀&看實況&日文");
+System.out.println("個性:樂觀/善良/天真");
+System.out.println("就讀大學&科系:亞洲大學資訊工程學系數位內容組");
+System.out.println("喜歡的歌手OR偶像OR樂團:天月/伊東歌詞太郎/CHICO/HoneyWorks");
+System.out.println("還請多多指教！！");
+	}
+}
